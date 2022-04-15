a2b803fc8881972fc6d276f35fb6d9bc14f29d66
Jokushu <65661802+Jokushu@users.noreply.github.com>
2022-04-15 00:47:49 +0000

    Modified model Menu and Category for relations

---
+++ app/models/categories_menu.rb
@@ -0,0 +1,4 @@
+class CategoriesMenu < ApplicationRecord
+	belongs_to :category
+	belongs_to :menu	
+end
--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,2 +1,4 @@
 class Menu < ApplicationRecord
+	has_many :categories_menus
+	has_many :categories, through: :categories_menus
 end

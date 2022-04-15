47924b6b3fab49bcd1f465142834bfe6d6e74f4a
Jokushu <65661802+Jokushu@users.noreply.github.com>
2022-04-15 11:48:07 +0000

    Add spec for Food, is valid with a name and a description

--- spec/models/menu_spec.rb
+++ spec/models/menu_spec.rb
@@ -1,7 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Menu, type: :model do
+	subject(:menu) do
+    return FactoryBot.create(:menu)
+  end
+	
 	it 'has a valid factory' do
     expect(FactoryBot.build(:menu)).to be_valid
+  end
+
+	it 'is valid with a name and a description' do
+    expect(menu).to be_valid
   end
 end

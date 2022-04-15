1689bf2acb48323952591a91be09c42fa7ac0ef6
Jokushu <65661802+Jokushu@users.noreply.github.com>
2022-04-15 11:50:53 +0000

    Add spec for menu, name can not be blank

--- spec/models/menu_spec.rb
+++ spec/models/menu_spec.rb
@@ -18,4 +18,10 @@ RSpec.describe Menu, type: :model do
     menu.valid?
     expect(menu.errors[:name]).to include("can't be blank")
   end
+
+	it 'is invalid without a blank name' do
+    menu.name = ''
+    menu.valid?
+    expect(menu.errors[:name]).to include("can't be blank")
+  end
 end

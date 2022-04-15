f894c16d57bc7787e976b267d88229195c5637a0
Jokushu <65661802+Jokushu@users.noreply.github.com>
2022-04-15 11:39:37 +0000

    Remove request and index rspec and set Food factory

--- spec/factories/menus.rb
+++ spec/factories/menus.rb
@@ -1,7 +1,13 @@
 FactoryBot.define do
-  factory :menu do
-    name { "MyString" }
-    description { "MyString" }
-    price { 1.5 }
+  factory :food do
+    name { Faker::Food.dish }
+    description { Faker::Food.description }
+    price { Faker::Commerce.price }
+  end
+
+  factory :invalid_food, parent: :food do
+    name { nil }
+    description { nil }
+    price { 10_000.0 }
   end
 end

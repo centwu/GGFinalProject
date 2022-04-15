66f149a4ce6f296634df8ae2147584f01f883a1d
Jokushu <65661802+Jokushu@users.noreply.github.com>
2022-04-15 00:36:17 +0000

    Generate migration for JOIN Menu table and Category table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_14_114530) do
+ActiveRecord::Schema.define(version: 2022_04_15_003440) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "categories_menus", id: false, force: :cascade do |t|
+    t.integer "category_id", null: false
+    t.integer "menu_id", null: false
+    t.index ["category_id", "menu_id"], name: "index_categories_menus_on_category_id_and_menu_id"
+    t.index ["menu_id", "category_id"], name: "index_categories_menus_on_menu_id_and_category_id"
+  end
 
   create_table "menus", force: :cascade do |t|
     t.string "name"

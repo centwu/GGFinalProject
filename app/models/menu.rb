d9a52e879939ecc7b7909ed172814f3b559571db
Jokushu <65661802+Jokushu@users.noreply.github.com>
2022-04-16 04:54:15 +0000

    Modified model Menu and Order for relations

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -2,6 +2,9 @@ class Menu < ApplicationRecord
 	has_many :categories_menus
 	has_many :categories, through: :categories_menus
 
+	has_many :menus_orders
+	has_many :orders, through: :menus_orders
+
 	validates :name, presence: true, uniqueness: true
 	validates :description, presence: true, length: { maximum: 150,
     too_long: "%{count} characters is the maximum allowed" }
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,4 @@
 class Order < ApplicationRecord
+	has_many :menus_orders
+	has_many :menus, through: :menus_orders
 end
